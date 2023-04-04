d5b1ea3786cea6a7f33649be2317c89ebcc51037
zhengfankai <zhengfankai@bytedance.com>
2023-04-05 05:34:52 +0800

    chore: implementation of web server

---
+++ web_server/src/lib.rs
@@ -0,0 +1,84 @@
+use std::{
+    sync::{
+        mpsc::{channel, Receiver, Sender},
+        Arc, Mutex,
+    },
+    thread,
+};
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: Option<Sender<Job>>,
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> Self {
+        assert!(size > 0);
+
+        let mut workers = Vec::with_capacity(size);
+        let (sender, receiver) = channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        for id in 0..size {
+            let receiver = Arc::clone(&receiver);
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        Self {
+            workers,
+            sender: Some(sender),
+        }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take().unwrap());
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<Receiver<Job>>>) -> Self {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv();
+
+            match job {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} was told to terminate.");
+                    break;
+                }
+            };
+        });
+
+        Self {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -1,3 +1,40 @@
+use std::io::{prelude::*, BufReader};
+use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
+use std::{fs, thread};
+
+use thread_pool::ThreadPool;
+
 fn main() {
-    println!("Hello, world!");
+    let server = TcpListener::bind("0.0.0.0:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for request in server.incoming().take(5) {
+        let stream = request.unwrap();
+
+        pool.execute(|| {
+            handle_conn(stream);
+        });
+    }
+}
+
+fn handle_conn(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+
+    let request_status_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status, uri) = match &request_status_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "static/index.html"),
+        _ => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 404 NOT FOUND", "static/404.html")
+        }
+    };
+
+    let contents = fs::read_to_string(uri).unwrap();
+    let length = contents.len();
+    let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
