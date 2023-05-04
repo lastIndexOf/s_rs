3fa14e40adca951586f28808414edc1bc99f1fd5
zhengfankai <zhengfankai@bytedance.com>
2023-05-04 22:42:37 +0800

    chore: std study

--- basic/std_study/src/main.rs
+++ basic/std_study/src/main.rs
@@ -1,8 +1,11 @@
 #![allow(unused_must_use)]
+#![allow(unused)]
 
 use std::{
     borrow::Borrow,
-    io::{BufRead, Write},
+    io::{BufRead, Read, Seek, Write},
+    num::{IntErrorKind, ParseIntError},
+    ops::{Add, Deref, Div},
 };
 
 fn main() {
@@ -19,6 +22,17 @@ fn main() {
     s_hint();
     s_io();
     s_iter();
+    s_marker();
+    s_mem();
+    s_net();
+    s_ops();
+    s_option();
+    s_os();
+    s_path();
+    s_process();
+
+    // 死灵书
+    s_repr();
 }
 
 fn s_ptr() {
@@ -265,10 +279,34 @@ fn s_io() -> std::io::Result<()> {
     //     .chain(std::io::stdin())
     //     .into_inner();
 
+    let mut main_rs = std::fs::File::open("./src/main.rs")?;
+    let mut content = [0; 20];
+
+    let size = main_rs.read(&mut content)?;
+    println!(
+        "main_rs content size = {size}, content = {:?}",
+        String::from_utf8(content.to_vec()).unwrap()
+    );
+
+    let size = main_rs.read(&mut content)?;
+    println!(
+        "main_rs content size = {size}, content = {:?}",
+        String::from_utf8(content.to_vec()).unwrap()
+    );
+
+    main_rs.seek(std::io::SeekFrom::Start(10000));
+
+    let size = main_rs.read(&mut content)?;
+    println!(
+        "main_rs content size = {size}, content = {:?}",
+        String::from_utf8(content.to_vec()).unwrap()
+    );
+
     Ok(())
 }
 
 fn s_iter() {
+    #[derive(Clone, Copy)]
     struct CounterIter {
         count: usize,
     }
@@ -276,6 +314,8 @@ fn s_iter() {
         fn new() -> Self {
             Self { count: 0 }
         }
+
+        pub const CONST_VAR: i32 = 32;
     }
     impl Iterator for CounterIter {
         type Item = usize;
@@ -420,4 +460,331 @@ fn s_iter() {
         "counter size = {:?}",
         counter.step_by(2).collect::<Vec<_>>()
     );
+
+    // node.js find, findIndex, indexOf, lastIndexOf
+    let counter = CounterIter::new();
+    let first_2 = counter.clone().find(|x| *x == 2);
+    let first_2_index = counter.clone().position(|x| x == 2);
+    let r_first_2_index = [1, 2, 3].iter().rposition(|x| *x == 3);
+
+    println!(
+        "first_2 = {:?}, first_2_index = {:?}, r_first_2_index = {:?}",
+        first_2, first_2_index, r_first_2_index,
+    );
+
+    println!("2.4 and 3.2 max is {}", f32::max(2.4, 3.2));
+    println!("2.4 and NAN max is {}", f32::max(2.4, f32::NAN));
+
+    // 比较浮点数的最大最小没法用 max, min 因为浮点数没有实现 Ord, 可以采用 reduce + f32::man / f32:: min
+    // let min = [2.4, f32::NAN, 3.2]
+    //     .into_iter()
+    //     .reduce(f32::max)
+    //     .ok_or(0.0_f32)
+    //     .unwrap();
+
+    let min = [2.4, f32::NAN, 3.2]
+        .iter()
+        .copied()
+        // .cloned()
+        .reduce(f32::max)
+        .ok_or(0.0_f32)
+        .unwrap();
+    println!("min = {min}");
+
+    let sum = [1.2, 2.3, 4.3, 5.4].iter().sum::<f32>();
+    println!("sum is {sum}");
+
+    let a = [1, 2, 3, 4, 5];
+    let b = [1, 2, 3, 4, 6];
+    let c = [3, 4, 6];
+    let d = [0, 1, 2];
+    let e = [1, 2, 3, 0, 0, 0];
+    let f = [1, 2, 3, 0, 0, 0, 0];
+
+    let cmp_less = a.iter().cmp(b.iter());
+    let cmp_great = c.iter().cmp(a.iter());
+    let eq = e.iter().eq(f.iter());
+    let ne = a.iter().ne(d.iter());
+    let less = b.iter().le(c.iter());
+    let great = c.iter().ge(a.iter());
+
+    println!("less = {cmp_less:?}");
+    println!("great = {cmp_great:?}");
+    println!("eq = {eq}");
+    println!("ne = {ne}");
+    println!("less = {less}");
+    println!("great = {great}");
+
+    println!("1 + 2 + 3 + 4 + 5 + 6 = {}", (1..=6).sum::<usize>());
+    println!("1 * 2 * 3 * 4 * 5 * 6 = {}", (1..=6).product::<usize>());
+
+    let _dirs = std::fs::read_dir("../")
+        .unwrap()
+        .map(|dir| dir.map(|entry| entry.path()))
+        .flatten()
+        // .chain(std::iter::once(std::path::PathBuf::from("~/.config")))
+        .chain(std::iter::once_with(|| {
+            std::path::PathBuf::from("~/.config")
+        }))
+        .collect::<Vec<_>>();
+
+    let _dirs = std::fs::read_dir("../")
+        .unwrap()
+        .map(|dir| dir.map(|entry| entry.path()))
+        .collect::<Result<Vec<_>, _>>();
+}
+
+fn s_marker() {
+    struct NoneCopy {}
+
+    let some = Some(5);
+    let _some_copied = some;
+    let _some_copied_twice = some;
+
+    let none_copy_some = Some(NoneCopy {});
+    let _none_copy_some_moved = none_copy_some;
+    // following will panic because of borrowing
+    // let none_copy_some_moved_twice = none_copy_some;
+
+    enum CopiedEnum {
+        Member1(i32),
+        Member2,
+    }
+    let one = CopiedEnum::Member1(32);
+    let two = CopiedEnum::Member2;
+
+    let _one_copied = one;
+    // let one_copied = one; // Error
+    let _two_copied = two;
+    let _two_copied = CopiedEnum::Member2;
+}
+
+fn s_mem() {
+    #[derive(Debug)]
+    struct StringPtr {
+        _ptr: String,
+    }
+
+    let str_ptr = StringPtr {
+        _ptr: String::new(),
+    };
+
+    println!(
+        "{str_ptr:?} align of is {}",
+        std::mem::align_of_val(&str_ptr)
+    );
+
+    println!("String align of is {}", std::mem::align_of::<String>());
+    println!("sizeof ptr is {}", std::mem::size_of::<&String>());
+
+    enum DiscriminateEnum {
+        EnumA,
+        EnumB,
+    }
+
+    assert_eq!(
+        std::mem::discriminant(&DiscriminateEnum::EnumA),
+        std::mem::discriminant(&DiscriminateEnum::EnumA)
+    );
+
+    assert_ne!(
+        std::mem::discriminant(&DiscriminateEnum::EnumB),
+        std::mem::discriminant(&DiscriminateEnum::EnumA)
+    );
+
+    let ptr = {
+        let ptr = Box::leak(Box::new(String::from("Hello, World")));
+        ptr
+    };
+
+    (*ptr).extend("with extend".chars());
+    println!("ptr = {}", *ptr);
+
+    unsafe {
+        let _ = Box::from_raw(ptr);
+        println!(
+            "ptr = {ptr:p}, ptr.as_ptr() = {:p}, ptr as *mut String = {:p}",
+            ptr.as_ptr(),
+            ptr as *mut String
+        );
+    }
+
+    println!("ptr = {}", *ptr);
+
+    let vector = vec![1, 2, 3];
+
+    println!(
+        "&vector = {:p}, &vector as *mut Vec = {:p}, vector.as_ptr() = {:p}",
+        &vector,
+        &vector as *const Vec<i32>,
+        vector.as_ptr()
+    );
+
+    let string = String::from("hello world");
+
+    println!(
+        "&string = {:p}, &string as *mut Vec = {:p}, string.as_ptr() = {:p}",
+        &string,
+        &string as *const String,
+        (&string).as_ptr()
+    );
+
+    println!("Box<i32> ptr = {:p}", Box::new(32_i32).deref());
+
+    let mut mem_ops_vec = vec![1, 2, 3, 4, 5];
+
+    std::mem::replace(&mut (mem_ops_vec[2]), 30);
+    println!("after replace vec = {mem_ops_vec:?}");
+
+    std::mem::take(&mut mem_ops_vec[2]);
+    println!("after take vec = {mem_ops_vec:?}");
+
+    std::mem::swap(&mut mem_ops_vec, &mut vec![1]);
+    println!("after swap vec = {mem_ops_vec:?}");
+}
+
+fn s_net() {
+    println!("(1..2).start = {}", (1..2).start);
+    println!("(1..2).end = {}", (1..2).end);
+    println!("(1..=2).end = {:?}", (1..=2));
+
+    let a = [1, 2, 3];
+    let b = a
+        .iter()
+        .copied()
+        .chain(std::iter::once(5))
+        .collect::<Vec<_>>();
+
+    let ipv4_addr = "0.0.0.0".parse::<std::net::Ipv4Addr>();
+    let socket_addr_v4 = "0.0.0.0:8080".parse::<std::net::SocketAddrV4>();
+
+    // ipv4_addr.unwrap().octets();
+    // socket_addr_v4.unwrap().ip();
+    // socket_addr_v4.unwrap().port();
+    // std::net::Ipv4Addr::LOCALHOST
+    // std::net::Ipv6Addr::LOCALHOST
+
+    println!("ipv4_addr = {ipv4_addr:?} socket_addr_v4 = {socket_addr_v4:?}");
+}
+
+fn s_ops() {
+    let range = (1.0_f64..2.0_f64);
+    // Error
+    // for i in (1.0_f64..2.0_f64) {
+    //     println!("i = {i}");
+    // }
+
+    println!("range contains 1.5 = {}", range.contains(&1.5));
+
+    // Infinity
+    println!(
+        "infinity range (1..) take 5 = {:?}",
+        (1..).take(10).collect::<Vec<_>>()
+    );
+}
+
+fn s_option() {
+    println!("4.0 / 2.0 = {}", 4.0.div(2.0));
+    println!("4.0 / 0.0 = {}", 4.0.div(0.0));
+
+    assert_eq!(f32::INFINITY, 4.0 / 0.0);
+
+    let a = "-91282223472332".parse::<i32>().or_else(|err| {
+        println!("err = {err}");
+        Err(err)
+    });
+
+    println!("a = {a:?} i16::MIN = {}", i16::MIN);
+
+    // fn make_iter(do_insert: bool) -> impl Iterator<Item = i32> {
+    //     // Explicit returns to illustrate return types not matching
+    //     match do_insert {
+    //         true => return (0..4).chain(std::iter::once(42)).chain(4..8),
+    //         false => return (0..4).chain(std::iter::empty::<i32>()).chain(4..8),
+    //     }
+    // }
+    fn make_iter(do_insert: bool) -> impl Iterator<Item = i32> {
+        match do_insert {
+            true => return (0..4).chain(Some(42)).chain(4..8),
+            false => return (0..4).chain(None).chain(4..8),
+        }
+    }
+
+    let mut some_string = Some(String::from("hello world"));
+    println!("some_string = {some_string:?}");
+
+    some_string.replace(String::from("changed"));
+    println!("some_string = {some_string:?}");
+
+    *(&mut some_string) = None;
+    println!("some_string = {some_string:?}");
+}
+
+fn s_os() {}
+
+fn s_path() {
+    let app_directory = std::env::current_dir();
+    let home = std::env::var("HOME");
+
+    println!("app_directory = {app_directory:?}, home = {home:?}");
+
+    let main = std::path::Path::new("./src/main.rs");
+
+    println!("main = {main:?}");
+    println!("main.file_stem = {:?}", main.file_stem());
+    println!("main.file_name = {:?}", main.file_name());
+    println!("main.extension = {:?}", main.extension());
+
+    let ancestors = main.ancestors().collect::<Vec<_>>();
+    println!("main.ancestors = {ancestors:?}");
+
+    assert_eq!(
+        main.strip_prefix("./"),
+        Ok(std::path::Path::new("src/main.rs"))
+    );
+
+    println!(
+        "{{./src/main.rs}}.canonicalize = {:?}",
+        std::path::Path::new("./src/main.rs").canonicalize()
+    );
+
+    println!("{{./src/main.rs}}.parent = {:?}", main.parent());
+
+    println!(
+        "{{./src/main.rs}}.parent = {:?}",
+        std::path::Path::new("./src/main.rs")
+            .canonicalize()
+            .unwrap()
+            .parent()
+    );
+
+    println!(
+        "./main.rs is exists = {}",
+        std::path::Path::new("./main.rs").exists()
+    );
+
+    println!(
+        "./main.rs is dir = {}",
+        std::path::Path::new("./main.rs").is_dir()
+    );
+
+    // alias to std::fs::metadata
+    println!(
+        "./src/main.rs metadata = {:?}",
+        std::path::Path::new("./src/main.rs").metadata()
+    );
+
+    let canonicalize = main.canonicalize().unwrap();
+    let without_app_dir = canonicalize.strip_prefix(std::env::current_dir().unwrap());
+
+    let current_dir = std::env::current_dir().unwrap();
+    let pth = <std::path::PathBuf as AsRef<std::path::Path>>::as_ref(&current_dir);
+
+    println!("without_app_dir = {without_app_dir:?}, pth = {pth:?}",);
+}
+
+fn s_process() {}
+
+fn s_repr() {
+    
 }
