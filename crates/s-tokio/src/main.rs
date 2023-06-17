2b1d66764150cdfc4cb752476ec27c0c62742623
cold_sharp <kgaikj2cu@icloud.com>
2023-06-18 02:59:29 +0800

    chore: s tokio

--- crates/s-tokio/src/main.rs
+++ crates/s-tokio/src/main.rs
@@ -1,5 +1,7 @@
 use std::time::Duration;
 
+use tokio_stream::StreamExt;
+
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
@@ -22,4 +24,74 @@ async fn main() {
             .unwrap()
             .count()
     );
+
+    let (tx1, rx1) = tokio::sync::oneshot::channel();
+    let (tx2, rx2) = tokio::sync::oneshot::channel();
+
+    tokio::spawn(async {
+        let _ = tx1.send("from one");
+    });
+
+    tokio::spawn(async {
+        let _ = tx2.send("from one");
+    });
+
+    tokio::select! {
+        val = rx1 => {
+            println!("from rx1 = {val:?}");
+        },
+        val = rx2 => {
+            println!("from rx1 = {val:?}");
+        },
+    };
+
+    let mut data = vec![0_u8; 1024];
+
+    tokio::select! {
+        _ = async {
+            let rc = std::rc::Rc::new(111);
+            tokio::time::sleep(Duration::from_secs(1)).await;
+            let data = data.clone();
+            println!("rc = {rc:?} data = {data:?}");
+        } => {
+            data.push(1);
+        },
+        _ = async {
+            let rc = std::rc::Rc::new(222);
+            tokio::time::sleep(Duration::from_secs(1)).await;
+            let data = data.clone();
+            println!("rc = {rc:?} data = {data:?}");
+        } => {
+            data.push(2);
+        },
+        _ = async {
+            let rc = std::rc::Rc::new(333);
+            tokio::time::sleep(Duration::from_secs(1)).await;
+            let data = data.clone();
+            println!("rc = {rc:?} data = {data:?}");
+        } => {
+            data.push(3);
+        },
+        else => {}
+    }
+
+    let _ = tokio::spawn(async move {
+        {
+            let rc = std::rc::Rc::new(111);
+            println!("rc = {rc:?}");
+        }
+        tokio::time::sleep(Duration::from_secs(1)).await;
+        let data = data.clone();
+        println!("data = {data:?}");
+        println!(" done ");
+    })
+    .await;
+
+    let itr = tokio_stream::iter(vec![1, 2, 3, 4, 5]).take(3);
+
+    tokio::pin!(itr);
+
+    while let Some(val) = itr.next().await {
+        println!("val = {val}");
+    }
 }
